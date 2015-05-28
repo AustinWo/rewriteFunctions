da9c38f62ab81be89e4ddede29f0f86a527aec3c
Austin Worachet <wworachet@gmail.com>
2015-05-27 22:38:14 -0700

    Tree.prototype.addChild

--- treeMap.js
+++ treeMap.js
@@ -6,3 +6,17 @@ var Tree = function(value){
   this.value = value;
   this.children = [];
 };
+
+Tree.prototype.addChild = function(child){
+  if (!child || !(child instanceof Tree)){
+    child = new Tree(child);
+  }
+
+  if(!this.isDescendant(child)){
+    this.children.push(child);
+  }else {
+    throw new Error("That child is already a child of this tree");
+  }
+  //return child node for convenience
+  return child;
+};
