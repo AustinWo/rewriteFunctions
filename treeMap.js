0d5fc3aca1293f3511e5f1b7df17d5bf16553c00
Austin Worachet <wworachet@gmail.com>
2015-05-27 22:38:48 -0700

    Tree.prototype.isDescendant

--- treeMap.js
+++ treeMap.js
@@ -20,3 +20,18 @@ Tree.prototype.addChild = function(child){
   //return child node for convenience
   return child;
 };
+
+Tree.prototype.isDescendant = function(child){
+  if(this.children.indexOf(child) !== -1){
+    // `child` is an immediate child of this tree
+    return true;
+  }else{
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].isDescendant(child)){
+        // `child` is descendant of this tree
+        return true;
+      }
+    }
+    return false;
+  }
+};
