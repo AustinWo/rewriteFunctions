b977339043b2380a35f8fcec1a42fff2429571db
Austin Worachet <wworachet@gmail.com>
2015-05-31 20:32:22 -0700

    telephone digits to letters

--- telephoneWords.js
+++ telephoneWords.js
@@ -26,3 +26,16 @@
   *  Why not filter your results to only return words contained in that file?
   *
   */
+
+var phoneDigitsToLetters = {
+  0: '0',
+  1: '1',
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ'
+};
